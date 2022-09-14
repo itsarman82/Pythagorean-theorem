0b9486a61e836d825669fbf67c762ee87711199e
arman sareskandi <42113160+itsarman82@users.noreply.github.com>
2022-09-15 02:54:01 +0430

    first commitment of Pythagorean theorem

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pythagorean_theorem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a simple application to solve Pythagorean theorem  
+
+            Console.WriteLine("Enter First Value: ");
+            double Value1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Value: ");
+            double Value2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = Math.Sqrt((Math.Pow(Value1, 2) + Math.Pow(Value2, 2)));
+            Console.WriteLine(result);
+
+        }
+    }
+}
